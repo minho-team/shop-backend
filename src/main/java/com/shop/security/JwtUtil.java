1e71eb1824d2b2adf5e294329829cb0c3e82d039
Minho Kang <busster@naver.com>
2026-03-17 11:14:45 +0900

    액세스토큰/쿠키 : 15분, 리프레시토큰/쿠키: 7일로 설정 후 테스트 완료

--- src/main/java/com/shop/security/JwtUtil.java
+++ src/main/java/com/shop/security/JwtUtil.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
-
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -16,87 +15,60 @@ import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtil {
-	
-	private final String SECRET_KEY="domino-my-super-key-12345160-domino-24843343";
-	
-	private final long EXPIRATION_TIME =1800000;
-	private final long REFRESH_EXPIRATION_TIME = 604800000;
-	
+
+	private final String SECRET_KEY = "domino-my-super-key-12345160-domino-24843343";
+
+	private final long EXPIRATION_TIME = 900000; // 액세스 토큰 15분
+	private final long REFRESH_EXPIRATION_TIME = 604800000; // 리프레시 토큰 7일
+
 	private Key getSigningKey() {
 		return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 	}
-	
-	//토큰생성
+
+	// 토큰생성
 	public String createToken(Authentication authentication) {
-		
+
 		String username = authentication.getName();
-		String roles = authentication.getAuthorities().stream()
-				.map(GrantedAuthority::getAuthority)
+		String roles = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority)
 				.collect(Collectors.joining(","));
 		Date now = new Date();
-		Date expiryDate = new Date(now.getTime()+EXPIRATION_TIME);
-		
-		return Jwts.builder()
-				.setSubject(username)
-				.claim("roles",roles)
-				.setIssuedAt(now)
-				.setExpiration(expiryDate)
-				.signWith(getSigningKey(),SignatureAlgorithm.HS256)
-				.compact();
-		
+		Date expiryDate = new Date(now.getTime() + EXPIRATION_TIME);
+
+		return Jwts.builder().setSubject(username).claim("roles", roles).setIssuedAt(now).setExpiration(expiryDate)
+				.signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
+
 	}
-	
-	
+
 	public String createRefreshToken(Authentication authentication) {
-		
-		String userId = authentication.getName();
-		
+
+		String memberId = authentication.getName();
+
 		Date now = new Date();
-		Date expiryDate = new Date(now.getTime()+REFRESH_EXPIRATION_TIME);
-		
-		return Jwts.builder()
-				.setSubject(userId)
-				.setIssuedAt(now)
-				.setExpiration(expiryDate)
-				.signWith(getSigningKey(),SignatureAlgorithm.HS256)
-				.compact();	
+		Date expiryDate = new Date(now.getTime() + REFRESH_EXPIRATION_TIME);
+
+		return Jwts.builder().setSubject(memberId).setIssuedAt(now).setExpiration(expiryDate)
+				.signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
 	}
-	
-	//토큰에서 이름 추출
-	public String getUserId(String token) {
-		return  Jwts.parserBuilder()
-                .setSigningKey(getSigningKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
+
+	// 토큰에서 이름 추출
+	public String getMemberId(String token) {
+		return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody().getSubject();
 	}
-	
+
 	// 토큰 유효성 검사
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parserBuilder()
-                    .setSigningKey(getSigningKey())
-                    .build()
-                    .parseClaimsJws(token);
-            return true;
-        } catch (JwtException | IllegalArgumentException e) {
-            return false;
-        }
-    }
-    // 토큰에서 권한 뽑아내기
-    public String[] getRoles(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getSigningKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .get("roles", String.class)
-                .split(",");
-    }
-	
-	
-	
-	
-	
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token);
+			return true;
+		} catch (JwtException | IllegalArgumentException e) {
+			return false;
+		}
+	}
+
+	// 토큰에서 권한 뽑아내기
+	public String[] getRoles(String token) {
+		return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody()
+				.get("roles", String.class).split(",");
+	}
+
 }
