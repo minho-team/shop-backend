1098b1ed3854a6238885b2b625c0f6ea411cb25b
xsaz0826 <xsaz0826@gmail.com>
2026-03-23 17:45:46 +0900

    장바구니에서 상품 구매시 장바구니에 있는 특정 상품 삭제 구현 및 주문 완료 페이지 상품 정보 구현

--- src/main/java/com/shop/dto/user/payment/PaymentConfirmRequestDTO.java
+++ src/main/java/com/shop/dto/user/payment/PaymentConfirmRequestDTO.java
@@ -1,10 +1,13 @@
 package com.shop.dto.user.payment;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class PaymentConfirmRequestDTO {
-    private String paymentKey;
-    private String orderId;
-    private Long amount;
+	private String paymentKey;
+	private String orderId;
+	private Long amount;
+	private List<Long> orderedCartItemNos;
 }
--- src/main/java/com/shop/dto/user/payment/PaymentConfirmResponseDTO.java
+++ src/main/java/com/shop/dto/user/payment/PaymentConfirmResponseDTO.java
@@ -1,14 +1,17 @@
 package com.shop.dto.user.payment;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class PaymentConfirmResponseDTO {
-    private Long orderNo;
-    private Long amount;
-    private String ordererName;
-    private LocalDateTime approvedAt;
+	private Long orderNo;
+	private Long amount;
+	private String ordererName;
+	private LocalDateTime approvedAt;
+	private List<PaymentPrepareItemDTO> items;
 }
